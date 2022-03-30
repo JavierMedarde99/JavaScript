10047a6b10dcb1ce7a74500049e7c9a8cfda2b76
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2022-03-30 22:41:02 +0200

    course of JavaScrip
    finish

---
+++ JS(ayax)/JSON/Ejercicio1/godos.php
@@ -0,0 +1,6 @@
+<?php
+
+$myJSON = '{"reyesgodos":[{"nombre":"Alarico","hijos":["Atanagildo","Leovigildo",{"nombre":"RecaredoI","hijos":["Sisebuto","RecaredoII",{"nombre":"Suintila","hijos":["Chindasvinto","Recesvinto","Wamba","Égica","Witiza","Rodrigo"]}]},"Sisenando","Chintila"]},"Witerico","Gundemaro"]}';
+
+echo $myJSON;
+?>
---
+++ JS(ayax)/JSON/prueba/godos.php
@@ -0,0 +1,6 @@
+<?php
+
+$vikings = '{"vikingos":[{"reino":"Noruega","reyes":["Hakon","Harald",["Ivar",["Olaf","Lars","Lasse"],"Bjorn"],"Ragnar"]},{"reino":"Dinamarca","reyes":["Frodo","Hamming",["Angantyr","Sarunas"],"Horik"]}]}';
+echo $vikings;
+
+?>
